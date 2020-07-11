76d0d29614252352cc3886b89d3c4b0b82bb9fd1
Reed Adams <radams@ctdi.com>
2020-07-11 11:29:02 -0400

    formatting

--- MessageQueue.Azure.Topic/AzureTopic.cs
+++ MessageQueue.Azure.Topic/AzureTopic.cs
@@ -8,34 +8,46 @@ namespace KM.MessageQueue.Azure.Topic
 {
     public sealed class AzureTopic<TMessage> : IMessageQueue<TMessage>
     {
-        private bool _Disposed = false;
-        private readonly TopicClient _TopicClient;
-        private readonly AzureTopicOptions<TMessage> _Options;
-        private readonly IMessageFormatter<TMessage> _Formatter;
+        private bool _disposed = false;
+        private readonly TopicClient _topicClient;
+        private readonly AzureTopicOptions<TMessage> _options;
+        private readonly IMessageFormatter<TMessage> _formatter;
 
-        private static readonly MessageAttributes _EmptyAttributes = new MessageAttributes();
+        private static readonly MessageAttributes _emptyAttributes = new MessageAttributes();
 
         public AzureTopic(IOptions<AzureTopicOptions<TMessage>> options, IMessageFormatter<TMessage> formatter)
         {
-            this._Options = options?.Value ?? throw new ArgumentNullException(nameof(options));
-            this._Formatter = formatter ?? throw new ArgumentNullException(nameof(formatter));
+            _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+            _formatter = formatter ?? throw new ArgumentNullException(nameof(formatter));
 
             var builder = new ServiceBusConnectionStringBuilder()
             {
-                Endpoint = this._Options.Endpoint,
-                EntityPath = this._Options.EntityPath,
-                SasKey = this._Options.SharedAccessKey,
-                SasKeyName = this._Options.SharedAccessKeyName,
-                TransportType = this._Options.TransportType
+                Endpoint = _options.Endpoint,
+                EntityPath = _options.EntityPath,
+                SasKey = _options.SharedAccessKey,
+                SasKeyName = _options.SharedAccessKeyName,
+                TransportType = _options.TransportType
             };
 
-            this._TopicClient = new TopicClient(builder);
+            _topicClient = new TopicClient(builder);
         }
 
-        public Task PostMessageAsync(TMessage message, CancellationToken cancellationToken) => this.PostMessageAsync(message, _EmptyAttributes, cancellationToken);
+        public Task PostMessageAsync(TMessage message, CancellationToken cancellationToken)
+        {
+            ThrowIfDisposed();
+
+            if (message is null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            return PostMessageAsync(message, _emptyAttributes, cancellationToken);
+        }
 
         public async Task PostMessageAsync(TMessage message, MessageAttributes attributes, CancellationToken cancellationToken)
         {
+            ThrowIfDisposed();
+
             if (message is null)
             {
                 throw new ArgumentNullException(nameof(message));
@@ -46,7 +58,7 @@ namespace KM.MessageQueue.Azure.Topic
                 throw new ArgumentNullException(nameof(attributes));
             }
 
-            var formattedMessageBytes = this._Formatter.Format(message);
+            var formattedMessageBytes = _formatter.Format(message);
 
             var topicMessage = new Message()
             {
@@ -63,44 +75,52 @@ namespace KM.MessageQueue.Azure.Topic
                 }
             }
 
-            await this._TopicClient.SendAsync(topicMessage);
+            await _topicClient.SendAsync(topicMessage);
+        }
+
+        private void ThrowIfDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(nameof(AzureTopic<TMessage>));
+            }
         }
 
         public void Dispose()
         {
-            this.Dispose(true);
+            Dispose(true);
             GC.SuppressFinalize(this);
         }
 
         private void Dispose(bool disposing)
         {
-            if (this._Disposed)
+            if (_disposed)
             {
                 return;
             }
 
             if (disposing)
             {
-                this._TopicClient.CloseAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+                _topicClient.CloseAsync().ConfigureAwait(false).GetAwaiter().GetResult();
             }
 
-            this._Disposed = true;
+            _disposed = true;
         }
 
-        ~AzureTopic() => this.Dispose(false);
+        ~AzureTopic() => Dispose(false);
 
 #if NETSTANDARD2_1
 
         // https://docs.microsoft.com/en-us/dotnet/standard/garbage-collection/implementing-disposeasync
         public async ValueTask DisposeAsync()
         {
-            if (this._Disposed)
+            if (_disposed)
             {
                 return;
             }
 
-            await _TopicClient.CloseAsync().ConfigureAwait(false);
-            this.Dispose(false);
+            await _topicClient.CloseAsync().ConfigureAwait(false);
+            Dispose(false);
             GC.SuppressFinalize(this);
         }
 
