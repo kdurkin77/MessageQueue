d850b55eb9e4c1f060324296f6773991f95b5c06
Reed Adams <radams@ctdi.com>
2020-07-13 15:51:16 -0400

    unused usings

--- MessageQueue.Specialized.Forwarder/ForwarderOptions.cs
+++ MessageQueue.Specialized.Forwarder/ForwarderOptions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace KM.MessageQueue.Specialized.Forwarder
+﻿namespace KM.MessageQueue.Specialized.Forwarder
 {
     public sealed class ForwarderOptions<TMessage>
     {
